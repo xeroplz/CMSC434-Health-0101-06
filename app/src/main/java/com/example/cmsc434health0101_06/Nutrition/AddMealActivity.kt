55b1f933a761bfc0d24ceaa201dd92b023b58033
Mohammed Arafat <amoh87@ymail.com>
2021-03-31 16:56:30 -0400

    adding nutrition activity

--- app/src/main/java/com/example/cmsc434health0101_06/Nutrition/AddMealActivity.kt
+++ app/src/main/java/com/example/cmsc434health0101_06/Nutrition/AddMealActivity.kt
@@ -2,6 +2,7 @@ package com.example.cmsc434health0101_06.Nutrition
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import com.example.cmsc434health0101_06.R
 
 class AddMealActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
--- app/src/main/java/com/example/cmsc434health0101_06/Nutrition/NutritionActivity.kt
+++ app/src/main/java/com/example/cmsc434health0101_06/Nutrition/NutritionActivity.kt
@@ -1,15 +1,54 @@
 package com.example.cmsc434health0101_06.Nutrition
 
 import android.os.Bundle
-import android.support.wearable.activity.WearableActivity
+import android.view.View
+import androidx.appcompat.app.AppCompatActivity
+import com.example.cmsc434health0101_06.R
+import com.github.mikephil.charting.charts.PieChart
+import com.github.mikephil.charting.data.Entry
+import com.github.mikephil.charting.data.PieData
+import com.github.mikephil.charting.data.PieDataSet
 
-class NutritionActivity : WearableActivity() {
+import java.security.KeyStore
 
+class NutritionActivity : AppCompatActivity() {
+
+
+
+    @Override
     override fun onCreate(savedInstanceState: Bundle?) {
+
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_nutrition)
 
-        // Enables Always-on
-        setAmbientEnabled()
+        setPieChart()
     }
+
+    fun setPieChart() {
+
+        val nutrientGraph = findViewById<PieChart>(R.id.nutrientGraph)
+        val nutrientType = ArrayList<String>()
+
+        nutrientType.add("Carbs")
+        nutrientType.add("Fats")
+        nutrientType.add("Protiens")
+
+        val nutrientValue = ArrayList<Entry>()
+        nutrientValue.add(Entry(22f, 0))
+        nutrientValue.add(Entry(43f, 1))
+        nutrientValue.add(Entry(35f, 2))
+
+
+
+        val pieDataSet = PieDataSet(
+            nutrientValue,
+            "Nutrients"
+        )
+
+        val data =  PieData(nutrientType, pieDataSet)
+
+        nutrientGraph.data = data
+
+    }
+
 }
