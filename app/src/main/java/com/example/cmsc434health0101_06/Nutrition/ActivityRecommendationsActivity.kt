a0fe61ac18a96c1dd245880a9995ab8e36d51d6c
Julian Travis <jtjonson16@gmail.com>
2021-04-03 21:07:58 -0400

    Added base files for meal manager and activity recommendation

---
+++ app/src/main/java/com/example/cmsc434health0101_06/Nutrition/ActivityRecommendationsActivity.kt
@@ -0,0 +1,12 @@
+package com.example.cmsc434health0101_06.Nutrition
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import com.example.cmsc434health0101_06.R
+
+class ActivityRecommendationsActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_recommendations)
+    }
+}
