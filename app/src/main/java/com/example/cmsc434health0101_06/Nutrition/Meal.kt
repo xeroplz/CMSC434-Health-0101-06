9d5b698384d22689184185bec630a2119affbcc9
Julian Travis <jtjonson16@gmail.com>
2021-04-07 15:03:00 -0400

    Set up json serialization for meals

--- app/src/main/java/com/example/cmsc434health0101_06/Nutrition/Meal.kt
+++ app/src/main/java/com/example/cmsc434health0101_06/Nutrition/Meal.kt
@@ -1,12 +1,25 @@
 package com.example.cmsc434health0101_06.Nutrition
 
+import android.util.Log
 import kotlin.collections.*;
 
-class Food(name: String, calories: Int, fats: Int, carbs: Int) {
+private val TAG = "HEALTH010106"
+
+class Tuple<K, T>(value1: K, value2: T) {
+    var value1 = value1
+    var value2 = value2
+
+    init {
+        this.value1 = value1
+        this.value2 = value2
+    }
+}
+
+class Food(name: String, calories: Int, fats: Double, carbs: Double) {
     lateinit var foodName: String
     var calories = 0
-    var fats = 0
-    var carbs = 0
+    var fats = 0.0
+    var carbs = 0.0
 
     init {
         this.foodName = name
@@ -16,12 +29,47 @@ class Food(name: String, calories: Int, fats: Int, carbs: Int) {
     }
 }
 
-class Meal(name: String) {
+class Meal(name: String, foods: ArrayList<Food>) {
     lateinit var mealName: String
-    private lateinit var foods: ArrayList<Food>
+    lateinit var foods: ArrayList<Food>
+
+    fun hasFood(foodName: String) : Tuple<Boolean, Int> {
+        val indices = this.foods.size - 1
+
+        (0..indices).forEach {
+            val foodItem = this.foods.get(it)
+            if (foodItem.foodName.equals(foodName)) {
+                return Tuple(true, it)
+            }
+        }
+
+        return Tuple(false, -1)
+    }
+
+    fun removeFood(foodName: String) {
+        val hasFoodResult = this.hasFood(foodName)
+
+        if (hasFoodResult.value1) {
+            this.foods.removeAt(hasFoodResult.value2)
+        } else {
+            Log.i(TAG, "Failed to remove '$foodName'. Food not found in meal.")
+            return
+        }
+    }
+
+    fun addFood(food: Food) {
+        val hasFoodResult = this.hasFood(food.foodName)
+
+        if (hasFoodResult.value1) {
+            Log.i(TAG, "Failed to add '${food.foodName}'. Food already exists in meal.")
+            return
+        } else {
+            this.foods.add(food)
+        }
+    }
 
     init {
         this.mealName = name
-        this.foods = arrayListOf<Food>()
+        this.foods = foods
     }
 }
--- app/src/main/java/com/example/cmsc434health0101_06/Nutrition/MealManagerActivity.kt
+++ app/src/main/java/com/example/cmsc434health0101_06/Nutrition/MealManagerActivity.kt
@@ -1,12 +1,72 @@
 package com.example.cmsc434health0101_06.Nutrition
 
-import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.os.Environment
+import android.util.Log
+import androidx.appcompat.app.AppCompatActivity
 import com.example.cmsc434health0101_06.R
+import java.io.*;
+import org.json.*;
+import com.google.gson.*;
 
 class MealManagerActivity : AppCompatActivity() {
+
+    private val fileName = "meals.json"
+    protected lateinit var myExternalFile: File
+    internal var myData = ""
+    private val isExternalStorageReadOnly: Boolean
+        get() {
+            val extStorageState = Environment.getExternalStorageState()
+            return if (Environment.MEDIA_MOUNTED_READ_ONLY == extStorageState) {
+                true
+            } else false
+        }
+
+    private val isExternalStorageAvailable: Boolean
+        get() {
+            val extStorageState = Environment.getExternalStorageState()
+            return if (Environment.MEDIA_MOUNTED == extStorageState) {
+                true
+            } else false
+        }
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_meal_manager)
+
+        // Test write data
+        val meals = ArrayList<Meal>()
+
+        val foods = ArrayList<Food>()
+        val banana = Food("Banana", 105, 0.4, 27.0)
+        val apple = Food("Apple", 95, 0.3, 25.0)
+        val smokedChicken = Food("Smoked Chicken", 320, 16.0, 11.0)
+        foods.addAll(arrayOf(banana, apple, smokedChicken))
+        val meal1 = Meal("Breakfast", foods)
+        meals.add(meal1)
+
+        val foods2 = ArrayList<Food>()
+        val salmon = Food("Salmon", 200, 11.4, 23.0)
+        val fries = Food("Fries", 320, 16.0, 48.0)
+        val bagel = Food("Bagel", 110, 7.0, 32.0)
+        foods2.addAll(arrayOf(salmon, fries, bagel))
+        val meal2 = Meal("Lunch", foods2)
+        meals.add(meal2)
+
+        val filePath = applicationContext.filesDir
+        Log.i(TAG, "Meals json located at $filePath/$fileName.")
+
+        // Serialize
+        val gson = Gson()
+        val str = gson.toJson(meals)
+
+        // Get file & write text
+        val outFile = File(filePath, fileName)
+        outFile.writeText(str)
+        Log.i(TAG, "Meal write success!")
+    }
+
+    companion object {
+        private val TAG = "HEALTH010106"
     }
 }
